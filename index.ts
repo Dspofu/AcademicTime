85e9d8d0735b64f015a9cc4dbc7bd98c00b63360
Dspofu <gabriel.pereira.goes01@gmail.com>
2025-10-13 21:41:36 -0300

    agora ele liga e eu adicionei mais uma dependencia

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("yep")
+import express from "express";
+
+const server = express();
+
+server.get("/", (req, res)=>{
+  res.end("<h1></h1>")
+})
+server.listen(8080, "0.0.0.0", ()=>console.log("Ta rodando essa jossa."))
